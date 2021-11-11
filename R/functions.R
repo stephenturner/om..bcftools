bc608ce61a3c655b5737f78718c780a98b29fc80
Stephen Turner <vustephen@gmail.com>
2021-11-11 08:49:39 -0500

    added tabix and bgzip

--- R/functions.R
+++ R/functions.R
@@ -14,3 +14,35 @@ bcftools <- function(...) {
   # run the command
   run(otsdr)
 }
+#' @name tabix
+#' @title tabix
+#' @description Run tabix
+#' @param ... Arguments
+#' @example /examples/example.R
+#' @details Find more help online \url{https://github.com/stephenturner/om..bcftools}.
+#' @export
+tabix <- function(...) {
+  # convert the ... into a argument list
+  arglist <- arglist_get(...)
+  # create an outsider object: describe the arguments and program
+  otsdr <- outsider_init(pkgnm = 'om..bcftools', cmd = 'tabix',
+                         arglist = arglist)
+  # run the command
+  run(otsdr)
+}
+#' @name bgzip
+#' @title bgzip
+#' @description Run bgzip
+#' @param ... Arguments
+#' @example /examples/example.R
+#' @details Find more help online \url{https://github.com/stephenturner/om..bcftools}.
+#' @export
+bgzip <- function(...) {
+  # convert the ... into a argument list
+  arglist <- arglist_get(...)
+  # create an outsider object: describe the arguments and program
+  otsdr <- outsider_init(pkgnm = 'om..bcftools', cmd = 'bgzip',
+                         arglist = arglist)
+  # run the command
+  run(otsdr)
+}
