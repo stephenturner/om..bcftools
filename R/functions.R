99acbf2244cc50fd9c33718b9140a3de648ece7b
Stephen Turner <vustephen@gmail.com>
2021-11-13 18:08:03 -0500

    fix checks

--- R/functions.R
+++ R/functions.R
@@ -5,15 +5,25 @@
 #' @example /examples/example.R
 #' @details Find more help online \url{https://github.com/stephenturner/om..bcftools}.
 #' @export
-bcftools <- function(...) {
-  # convert the ... into a argument list
-  arglist <- arglist_get(...)
-  # create an outsider object: describe the arguments and program
+bcftools <-   function(...) {
+  arglist = arglist_get(...)
+  files_to_send <- filestosend_get(arglist)
+  # Find the vcf.gz from the list of files.
+  vcfgz <- grep("vcf\\.gz$", files_to_send, value=TRUE)
+  # Send the tabix index along with it
+  if(length(vcfgz)>0) files_to_send <- c(files_to_send, paste0(vcfgz[length(vcfgz)], ".tbi"))
+  # Set the working dir to the directory where the vcf.gz lives
+  working_dir <- normalizePath(dirname(vcfgz))
+  # Get the rest of the non-file arguments
+  arglist <- arglist_parse(arglist)
+  # Construct the outsider object
   otsdr <- outsider_init(pkgnm = 'om..bcftools', cmd = 'bcftools',
+                         wd = working_dir, files_to_send = files_to_send,
                          arglist = arglist)
-  # run the command
+  # Run it
   run(otsdr)
 }
+
 #' @name tabix
 #' @title tabix
 #' @description Run tabix
@@ -21,11 +31,15 @@ bcftools <- function(...) {
 #' @example /examples/example.R
 #' @details Find more help online \url{https://github.com/stephenturner/om..bcftools}.
 #' @export
-tabix <- function(arglist = arglist_get(...), outdir = getwd()) {
+tabix <- function(...) {
+  arglist = arglist_get(...)
   files_to_send <- filestosend_get(arglist)
+  if (length(files_to_send)>1L) stop("Usage: tabix('/path/to/vcf.gz')")
+  if (length(files_to_send)==1L && !grepl("\\.vcf\\.gz$", files_to_send)) stop("Usage: tabix('/path/to/vcf.gz')")
+  working_dir <- normalizePath(dirname(files_to_send))
   arglist <- arglist_parse(arglist)
   otsdr <- outsider_init(pkgnm = 'om..bcftools', cmd = 'tabix',
-                         wd = outdir, files_to_send = files_to_send,
+                         wd = working_dir, files_to_send = files_to_send,
                          arglist = arglist)
   run(otsdr)
 }
@@ -36,11 +50,15 @@ tabix <- function(arglist = arglist_get(...), outdir = getwd()) {
 #' @example /examples/example.R
 #' @details Find more help online \url{https://github.com/stephenturner/om..bcftools}.
 #' @export
-bgzip <-   function(arglist = arglist_get(...), outdir = getwd()) {
+bgzip <- function(...) {
+  arglist <- arglist_get(...)
   files_to_send <- filestosend_get(arglist)
+  if (length(files_to_send)>1L) stop("Usage: bgzip('/path/to/vcf')")
+  if (length(files_to_send)==1L && !grepl("\\.vcf$", files_to_send)) stop("Usage: bgzip('/path/to/vcf')")
+  working_dir <- normalizePath(dirname(files_to_send))
   arglist <- arglist_parse(arglist)
   otsdr <- outsider_init(pkgnm = 'om..bcftools', cmd = 'bgzip',
-                         wd = outdir, files_to_send = files_to_send,
+                         wd = working_dir, files_to_send = files_to_send,
                          arglist = arglist)
   run(otsdr)
 }
--- scratch.R
+++ scratch.R
@@ -1,17 +1,31 @@
-bcftools <-   function(...) {
+bcftools("view", "-r", "2", "inst/extdata/example.vcf.gz")
+bcftools("view", "-r", "2", "inst/extdata/example.vcf.gz", "-Ov", "-o", "2.vcf")
+bgzip("inst/extdata/2.vcf")
+tabix("-f", "vcf", "inst/extdata/2.vcf.gz")
+
+bcftools <-   function(..., output_file=NULL) {
   arglist = arglist_get(...)
   files_to_send <- filestosend_get(arglist)
+  # Find the vcf.gz from the list of files.
   vcfgz <- grep("vcf\\.gz$", files_to_send, value=TRUE)
+  # Send the tabix index along with it
   if(length(vcfgz)>0) files_to_send <- c(files_to_send, paste0(vcfgz[length(vcfgz)], ".tbi"))
+  # Set the working dir to the directory where the vcf.gz lives
   working_dir <- normalizePath(dirname(vcfgz))
+  # Get the rest of the non-file arguments
   arglist <- arglist_parse(arglist)
+  if (!is.null(output_file)) {
+    # write out the results of figlet to output_file
+    arglist <- c(arglist, '>', basename(output_file))
+  }
+  # Construct the outsider object
   otsdr <- outsider_init(pkgnm = 'om..bcftools', cmd = 'bcftools',
                          wd = working_dir, files_to_send = files_to_send,
                          arglist = arglist)
+  # Run it
   run(otsdr)
 }
 
-arglist <- c("view", "-r one", "inst/extdata/test.vcf")
-bgzip("inst/extdata/test.vcf", outdir="inst/extdata/")
-tabix(arglist = c("-f", "inst/extdata/test.vcf.gz"), outdir="inst/extdata")
-bcftools("view", "-r", "one", "inst/extdata/test.vcf.gz")
+bcftools("query", "inst/extdata/2.vcf.gz", "-f", "%CHROM\\t%POS\\t%ID\\t[%TGT]\\n", output_file = "2.tsv")
+arglist <- c("query", "inst/extdata/2.vcf.gz", "-f", "%CHROM\\t%POS\\t%ID\\t[%TGT]\\n")
+output_file = "2.tsv"
